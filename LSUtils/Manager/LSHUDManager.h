a57aff74dbb30c74588cb5f4a12d52825d64f1aa
sandro <liushuai_ios@126.com>
2019-01-03 18:09:32 +0800

    去掉非空宏

--- LSUtils/Manager/LSHUDManager.h
+++ LSUtils/Manager/LSHUDManager.h
@@ -8,8 +8,6 @@
 
 #import <Foundation/Foundation.h>
 
-NS_ASSUME_NONNULL_BEGIN
-
 @interface LSHUDManager : NSObject
 
 /**
@@ -69,5 +67,3 @@ NS_ASSUME_NONNULL_BEGIN
  */
 + (void)dissmissSVHUDAfterTime:(NSTimeInterval)time completion:(void(^)(void))completion;
 @end
-
-NS_ASSUME_NONNULL_END
